fc5170ff2bc5f82709430a4d1de6d0fa40947c28
auw2025 <auw@tsss.edu.hk>
2025-06-20 09:34:43 +0800

    Make the button Prussian blue

--- lib/screens/onboding/components/animated_btn.dart
+++ lib/screens/onboding/components/animated_btn.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:rive/rive.dart';
+import 'package:rive/rive.dart' hide LinearGradient;
 
 class AnimatedBtn extends StatelessWidget {
   const AnimatedBtn({
@@ -21,25 +21,71 @@ class AnimatedBtn extends StatelessWidget {
         width: 236,
         child: Stack(
           children: [
-            RiveAnimation.asset(
-              "assets/RiveAssets/button.riv",
-              controllers: [_btnAnimationController],
-            ),
-            Positioned.fill(
-              top: 8,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const Icon(CupertinoIcons.arrow_right),
-                  const SizedBox(width: 8),
-                  Text(
-                    "Start the course",
-                    style: Theme.of(context).textTheme.labelLarge,
-                  )
-                ],
+            // Base container with Prussian blue
+            Container(
+              height: 64,
+              width: 236,
+              decoration: BoxDecoration(
+                color: const Color(0xFF003153),
+                borderRadius: BorderRadius.circular(30),
               ),
-            )
+            ),
+            // ShaderMask to tint the Rive asset completely in Prussian blue.
+            ShaderMask(
+              shaderCallback: (Rect bounds) {
+                return const LinearGradient(
+                  colors: [Color(0xFF003153)],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ).createShader(bounds);
+              },
+              blendMode: BlendMode.srcIn,
+              child: RiveAnimation.asset(
+                "assets/RiveAssets/button.riv",
+                controllers: [_btnAnimationController],
+              ),
+            ),
+            // Centered badge for text and icon on top.
+            Positioned.fill(
+              child: Center(
+                child: Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  decoration: BoxDecoration(
+                    color: const Color(0xFF003153),
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const Icon(
+                        CupertinoIcons.arrow_right,
+                        color: Colors.white,
+                      ),
+                      const SizedBox(width: 8),
+                      Text(
+                        "Start the course",
+                        style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              shadows: const [
+                                Shadow(
+                                  offset: Offset(1, 1),
+                                  blurRadius: 4,
+                                  color: Colors.black45,
+                                ),
+                                Shadow(
+                                  offset: Offset(-1, -1),
+                                  blurRadius: 4,
+                                  color: Colors.black45,
+                                ),
+                              ],
+                            ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
